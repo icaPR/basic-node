14bb3b0ba7dc7dba6fd1c41e23a20a36cb524bb5
Icaro Ramos <polosnramos@hotmail.com>
2024-04-23 17:10:38 -0300

    feat: Update server.js with route handling logic

--- src/server.js
+++ src/server.js
@@ -1,5 +1,28 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
 
-server.listen(3333);
+  await json(req, res);
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
+
+  if (route) {
+    const routeParams = req.url.match(route.path);
+
+    const { query, ...params } = routeParams.groups;
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
+    return route.handler(req, res);
+  }
+
+  return;
+});
+
+server.listen(3334);
